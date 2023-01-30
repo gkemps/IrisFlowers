16ec6d18bd9f73ef434ae2232e86359da1c63921
geert <geert@route42.nl>
2023-01-30 13:07:56 +0100

    logging

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import joblib
+import logging
 from fastapi import FastAPI
 from flower_iris import GetFlowerIrisClassRequest
 from flower_iris import FlowerIrisResponseData
@@ -11,6 +12,10 @@ pkl_filename = "model/pipple_model.pkl"
 with open(pkl_filename, 'rb') as file:
  model = joblib.load(file)
 
+#logger
+logger = logging.getLogger("uvicorn") 
+logger.setLevel(logging.INFO)
+
 # create post /GetFlowerIrisClass method
 @app.post('/GetFlowerIrisClass')
 def predict_species(data: GetFlowerIrisClassRequest):
@@ -22,6 +27,16 @@ def predict_species(data: GetFlowerIrisClassRequest):
         ]])
 
     if prediction.size < 1 or prediction[0] < 0 or prediction[0] > 2:
+        logger.error(
+            "FlowerId: %s SepalLength: %.2f SepalWidth: %.2f PedalLength: %.2f PedalWidth: %.2f; Prediction: %s",
+            data.FlowerIrisRequestData.FlowerID,
+            data.FlowerIrisRequestData.SepalLength,
+            data.FlowerIrisRequestData.SepalWidth,
+            data.FlowerIrisRequestData.PetalLength,
+            data.FlowerIrisRequestData.PetalWidth,
+            prediction
+        )  
+
         return {
             'FlowerIrisResponseData': {
                 'FlowerID': data.FlowerIrisRequestData.FlowerID,
@@ -36,7 +51,18 @@ def predict_species(data: GetFlowerIrisClassRequest):
     if prediction[0] == 1:
         irisClass = "Versicolor"
     if prediction[0] == 2:
-        irisClass = "Virginica"          
+        irisClass = "Virginica"    
+
+    logger.info(
+        "FlowerId: %s SepalLength: %.2f SepalWidth: %.2f PedalLength: %.2f PedalWidth: %.2f; Prediction: %s = %s",
+        data.FlowerIrisRequestData.FlowerID,
+        data.FlowerIrisRequestData.SepalLength,
+        data.FlowerIrisRequestData.SepalWidth,
+        data.FlowerIrisRequestData.PetalLength,
+        data.FlowerIrisRequestData.PetalWidth,
+        prediction,
+        irisClass
+    )          
 
     responseData = FlowerIrisResponseData(
         FlowerID=data.FlowerIrisRequestData.FlowerID,
